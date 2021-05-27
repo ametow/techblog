bb41e48816160f7b34b4ccb6ce14f450bacffdc7
Arslan <dev.ametov@gmail.com>
2021-05-28 00:28:20 +0500

    nojekyll

--- next.config.js
+++ next.config.js
@@ -1,20 +1,7 @@
 // next.config.js
-const withPlugins = require("next-compose-plugins");
-const optimizedImages = require("next-optimized-images");
 module.export = {
   images: {
     domains: ["127.0.0.1"],
   },
   optimizeImages: false,
 };
-// module.exports = withPlugins([
-//   [
-//     optimizedImages,
-//     {
-//       images: {
-//         domains: ["127.0.0.1"],
-//       },
-//       optimizeImages: false,
-//     },
-//   ],
-// ]);
