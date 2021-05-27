08d437a35d46de48f67d0f5650458c062e992765
Arslan <dev.ametov@gmail.com>
2021-05-28 00:35:36 +0500

    asdf

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 // next.config.js
+const basePath = process.env.NODE_ENV === "production" ? "/repo" : "";
 module.export = {
   images: {
     domains: ["127.0.0.1"],
   },
   optimizeImages: false,
+  basePath: "/repo",
+  assetPrefix: "/repo/",
 };
